2ad9a83a83eab476ebaa92701dc2960aabf34851
ldonita <ldonita@student.21-school.ru>
2021-03-20 03:28:48 +0300

    check_res todo tests

--- srcs/parser.c
+++ srcs/parser.c
@@ -25,6 +25,37 @@
 
 #define IDS		{RES, AMB, CAM, LS, PL, SP, SQ, CY, TR, NULL}
 
+/*
+
+//
+///
+subj:
+- Elements which are defined by a capital letter
+ can only be declared once in the scene.
+
+- Each element first’s information is the type identifier
+ (composed by one or two character(s)),
+  followed by all specific information for each object in a STRICT ORDER
+
+- Each type of element CAN be separated by one or more line break(s).
+-Each type of information from an element CAN be separated by one or more
+space(s).
+
+///
+NOTE: Валиден ли такой инпут?:
+R19201080A0.2255,255,255c-50,0,200,0,070
+Вроде нет,потому что здесь нельзя ничего неоднозначно распарсить
+
+Идем из допущения, что пробелы - разделитель между параметрами элемента сцены, 
+а новые строки - между элементами сцены
+//
+
+RES: 2 positive numbers
+
+
+
+*/
+
 void	init_rt(t_rt *rt)
 {
 	rt->res		= NULL;
@@ -38,6 +69,40 @@ int		check_rt(t_rt *rt)
 {
 	return (rt->res || rt->lamb || rt->shapes || rt->lsrcs || rt->cams);
 }
+
+int		check_res (char **tokens, t_rt *rt)
+{
+	double X;
+	double Y;
+	int i;
+	int n;
+
+	n = 3;
+	i = 0;
+	if (rt->res)
+		return (printf("RESOLUION IS ALREADY SET!\n"));
+	while (tokens[i] != NULL)
+		if (i > n)
+			return (printf("TOO MANY ELEMENTS IN RESOLUTION"));
+	if (i < n)
+		return (printf("TOO FEW ELEMENTS IN RESOLUTION"));
+	if (isnan(X = (double)ft_atof((*tokens)[1])) || (int)X <= 0)
+		return (printf("BAD WINDOW WIDTH"));
+	if (isnan(Y = (double)ft_atof((*tokens)[1])) || (int)Y <= 0)
+		return (printf("BAD WINDOW HEIGHT"));
+	// TODO: silently adjust resolution to max_screen_size
+	rt->res = &(t_res){(int)X, (int)Y};
+	return (1);
+}
+int		check_amb(char **tokens, t_rt *rt)
+{
+	double lum;
+	int r;
+	int g; 
+	int b;
+
+}
+
 
 void	parser(char *path)
 {
