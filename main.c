9f5ba83b6ebf150689e3c889157ae12bcac3b0ed
ldonita <ldonita@student.21-school.ru>
2021-04-26 17:37:32 +0300

    see comments

--- main.c
+++ main.c
@@ -22,7 +22,6 @@ int main(int argc, char **argv)
 		out = parser("1.rt", &rt);
 		if (out == -1)
 			return (handle_malloc());
-			// TODO: check for invalid file or folder
 			// TODO: one more time for the leaks
 
 		if (out != 1)
