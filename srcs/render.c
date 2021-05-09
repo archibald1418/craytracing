d4d786649053376d19c5b57ada7396c404995edb
ldonita <ldonita@student.21-school.ru>
2021-05-09 03:47:40 +0300

    pixel rate added

--- srcs/render.c
+++ srcs/render.c
@@ -7,23 +7,25 @@ ssize_t render(t_conf *conf, t_rt *rt, t_cam cam)
     int i;
     int j;
     t_ray ray;
+    int rate;
 
     j = 0;
+    rate = 2;
     color = black;
     cam.rot = get_rotation(cam);
-    while (j < conf->res.Y)
+    while (j <= conf->res.Y - rate)
     {
         i = 0;
-        while (i < conf->res.X)
+        while (i <= conf->res.X - rate)
         {
             init_ray(&ray, &conf->res, i, j, cam);
             color = trace_shapes(ray, rt->shapes, &orient);
             if (color != black)
                 color = calc_lights(color, orient, rt);
             my_mlx_pixel_put(&conf->img, i, j, color);
-            i++;
+            i += rate;
         }
-        j++;
+        j += rate;
     }
     return (1);
 }
