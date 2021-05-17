f24fb48f99f91ebe5c7fd271c29647672bd70c6c
ldonita <ldonita@student.21-school.ru>
2021-05-17 16:13:56 +0300

    setters.c norme ok

--- srcs/setters.c
+++ srcs/setters.c
@@ -1,13 +1,13 @@
 #include "minirt.h"
 
-void		set_color(t_color *col, int r, int g, int b)
+void	set_color(t_color *col, int r, int g, int b)
 {
 	col->r = r;
 	col->g = g;
 	col->b = b;
 }
 
-void		set_lamb(t_lamb *lamb, double lum, t_color *col)
+void	set_lamb(t_lamb *lamb, double lum, t_color *col)
 {
 	lamb->lum = lum;
 	lamb->col.r = col->r;
@@ -15,16 +15,13 @@ void		set_lamb(t_lamb *lamb, double lum, t_color *col)
 	lamb->col.b = col->b;
 }
 
-void		set_lsrc(t_lsrc *lsrc, double lum, t_color col, t_p3d p)
+void	set_lsrc(t_lsrc *lsrc, double lum, t_color col, t_p3d p)
 {
 	lsrc->lum = lum;
-
 	lsrc->col.r = col.r;
 	lsrc->col.g = col.g;
 	lsrc->col.b = col.b;
-
 	lsrc->loc.x = p.x;
 	lsrc->loc.x = p.y;
 	lsrc->loc.x = p.z;
 }
-
