d182120eb8807598df399713691955c8dbba27f6
ldonita <ldonita@student.21-school.ru>
2021-05-17 16:22:49 +0300

    utils norme ok

--- srcs/utils.c
+++ srcs/utils.c
@@ -5,9 +5,10 @@ double	get_min_pos_root(double disc, double a, double b)
 	double	r1;
 	double	r2;
 	double	sqd;
+
 	sqd = sqrt(disc);
-	r1 = (-b + sqd)/(2 * a);
-	r2 = (-b - sqd)/(2 * a);
+	r1 = (-b + sqd) / (2 * a);
+	r2 = (-b - sqd) / (2 * a);
 	if (r1 >= 0 && r2 >= 0)
 		return (fmin(r1, r2));
 	if (r1 >= 0)
